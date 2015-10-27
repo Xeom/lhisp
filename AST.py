2ffbf4f14df1b4d399e83191a69d0464281f923b
Michael Andrews <mandrews@wpi.edu>
2015-10-27 17:29:17 -0400

    Moar AST

---
+++ AST.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# AST node.
+class HNode:
+
+    types = []
+
+    def getnode(text):
+        for i in types:
+            if i.match(text) == True:
+                return i.fromtext(types)
+
+    def fromtext(text):
+        raise NotImplementedError()
+
+    def match(text):
+        raise NotImplementedError()
+
+class HList(HNode):
+
+    def fromtext(text):
+        text = text[1:-1]
+        children = []
+        while len(text):
+            atom, size = (text)
+            children += atom
+            text = text[size:]
+            text.strip()
+
+        return HList(children)
+
+    def match(text):
+        pass
